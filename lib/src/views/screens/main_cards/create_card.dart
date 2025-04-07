2651a03637abc8030b215e71702230436b78d6f4
Averoes <babar_ali@averroestudios.com>
2025-04-07 17:19:51 +0500

    Qr Scanner features and design.

--- lib/src/views/screens/main_cards/create_card.dart
+++ lib/src/views/screens/main_cards/create_card.dart
@@ -7,8 +7,32 @@ class CreateCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF2F7F5),
-      body: Center(
-        child: Text("Create Card"),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.2,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15, top: 35, right: 35),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Icon(Icons.arrow_back, color: Colors.white),
+                    Expanded(child: Center(child: Text("V card", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),))),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/src/views/screens/main_cards/create_event.dart
+++ lib/src/views/screens/main_cards/create_event.dart
@@ -10,109 +10,144 @@ class CreateEvent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF2F7F5),
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.2,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15, top: 50),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Icon(Icons.arrow_back, color: Colors.white),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Transform.translate(
-              offset: Offset(0, -70),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.4,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.2,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 20),
-                      SocialWidget(
-                        iconPath: "assets/icons/calendar_svg.svg",
-                        label: "Event",
-                      ),
-                      SizedBox(height: 10),
-                      DashboardLabel("Title"),
-                      TextField(
-                        keyboardType: TextInputType.numberWithOptions(
-                          decimal: true,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, top: 50),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Icon(Icons.arrow_back, color: Colors.white),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Transform.translate(
+                offset: Offset(0, -70),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+        
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 20),
+                        SocialWidget(
+                          iconPath: "assets/icons/calendar_svg.svg",
+                          label: "Event",
                         ),
-                        maxLength: 10,
-                        decoration: InputDecoration(
-                          hintText: "Please enter the title",
-                          counterText: '',
-                          hintStyle: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
+                        SizedBox(height: 10),
+                        DashboardLabel("Title"),
+                        TextField(
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                            hintText: "Please enter the title",
+                            counterText: '',
+                            hintStyle: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      DashboardLabel("Location"),
-                      TextField(
-                        keyboardType: TextInputType.numberWithOptions(
-                          decimal: true,
-                        ),
-                        maxLength: 10,
-                        decoration: InputDecoration(
-                          hintText: "Please enter the Location",
-                          counterText: '',
-                          hintStyle: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
+                        SizedBox(height: 10),
+                        DashboardLabel("Location"),
+                        TextField(
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                            hintText: "Please enter the Location",
+                            counterText: '',
+                            hintStyle: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      DashboardLabel("Description"),
-                      TextField(
-                        keyboardType: TextInputType.numberWithOptions(
-                          decimal: true,
-                        ),
-                        maxLength: 10,
-                        decoration: InputDecoration(
-                          hintText: "Please enter the description",
-                          counterText: '',
-                          hintStyle: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
+                        SizedBox(height: 10),
+                        DashboardLabel("Description"),
+                        TextField(
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                            hintText: "Please enter the description",
+                            counterText: '',
+                            hintStyle: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 10),
+                        DashboardLabel("Start Date"),
+                        TextField(
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                            hintText: "Please enter the date",
+                            counterText: '',
+                            hintStyle: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        DashboardLabel("End Date"),
+                        TextField(
+                          keyboardType: TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          maxLength: 10,
+                          decoration: InputDecoration(
+                            hintText: "Please enter the end date",
+                            counterText: '',
+                            hintStyle: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 30, right: 30),
-              child: Text("Create", style: TextStyle(color: Colors.white),),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: Text("Create", style: TextStyle(color: Colors.white),),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
